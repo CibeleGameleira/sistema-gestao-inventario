98b8f06ffe707f812f5857edf19500420abb9d0c
Cibele Mendes Gameleira <cibelewbcmendes@gmail.com>
2024-11-27 19:34:55 -0300

    feat(dicionario-listas): Adiciona classe que representa os produtos

---
+++ src/models/produto.py
@@ -0,0 +1,7 @@
+class Produto: 
+    def __init__(id, nomem, quantidade, preco, importado):
+        self.id = id
+        self.nome = nomem
+        self.quantidade = quantidade
+        self.preco = preco
+        self.importado = importado
