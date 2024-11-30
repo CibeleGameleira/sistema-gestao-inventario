55142bd84d04946f8342467a40cf55ef53d458c9
CibeleGameleira <cibelewbcmendes@gmail.com>
2024-11-30 01:39:37 -0300

    feat(dicionario e listas): adiciona comentários que servirão como documentação do código e refactoring da função de atualização de produto
    feat(dicionario e listas): adiciona comentários que servirão como documentação do código e refactoring da função de atualização de produto

--- src/models/produto.py
+++ src/models/produto.py
@@ -1,9 +1,12 @@
+#Define a classe produto
 class Produto:
+    #Define o método construtor, para construir a instância do produto. 
     def __init__(self, nome: str, quantidade: int, preco: float, importado: bool):
         self.nome: str = nome
         self.quantidade: int = quantidade
         self.preco: float = preco
         self.importado: bool = importado
-
+    
+    #Esse método irá retornar uma representação da instância 
     def __repr__(self):
         return f"nome: {self.nome}, quantidade: {self.quantidade}, preco: {self.preco}, importado: {self.importado}"
--- src/services/dicionario.py
+++ src/services/dicionario.py
@@ -3,27 +3,51 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from models.produto import Produto
 
+# Classe que representa o Dicionário de Produtos
 class Dicionario: 
-    estoque = {}
+    estoque = {} #dicionário
+    
+    # Essa função é responsável por inicializar o dicionário, adicionando em um laço de repetição instâncias de produtos
+    # Essa função é um método estático, pois não precisa de um objeto para ser chamado.  
+   
     @staticmethod
-    def inicializarDicionarioProdutos (): 
+    def inicializarDicionarioProdutos():
          for i in range(1, 50):
             produto = Produto(f"Produto {i}", 1, 0.0, False)
             Dicionario.estoque[i] = produto
 
+    #Essa função é responsável por adicionar um produto baseado no que foi passado como parâmetro para função
+    #Essa função valida o ID do último produto, adicionando em uma nova posição o produto
+    #Essa função é um método estático, pois não precisa de um objeto para ser chamado.  
+
     @staticmethod
-    def adicionarProduto (produto: Produto):
+    def adicionarProduto(produto: Produto):
         ultimoProduto = list(Dicionario.estoque.keys())[-1]
         Dicionario.estoque[ultimoProduto + 1] = produto
 
+    #Essa função é responsável por atualizar um produto baseado no ID passado como parâmetro para função
+    #Essa função é um método estático, pois não precisa de um objeto para ser chamado.  
+    #Essa função atualiza o produto na posição correspondente do dicionário, podendo atualizar as propriedades quantidade e preço
+    #Caso não seja passado um novo valor para quantidade ou preço, a função não altera o valor da respectiva propriedade do produto.    
+    #Essa função também valida se é encontrado o produto com o ID passado por parâmetro
     @staticmethod
-    def atualizarProduto (id, produto: Produto):
-        Dicionario.estoque[id] = produto
+    def atualizarProduto(id,quantidade=None, preco=None):
+        if id in Dicionario.estoque:
+            if  quantidade is not None:
+                Dicionario.estoque[id].quantidade = quantidade
+            if preco is not None:
+                Dicionario.estoque[id].preco = preco
+
+    #Essa função é responsável por remover um produto baseado no ID passado como parâmetro para função
+    #Essa função é um método estático, pois não precisa de um objeto para ser chamado.  
 
     @staticmethod
-    def removerProduto (id):
+    def removerProduto(id):
         Dicionario.estoque.pop(id, None) 
 
+    #Essa função é responsável por buscar um produto baseado no ID ou nome passado como parâmetro para função
+    #Essa função retorna o produto caso seja encontrado, caso contrário, retorna uma mensagem de produto não encontrado
+    #Essa função é um método estático, pois não precisa de um objeto para ser chamado.  
     @staticmethod
     def buscarProduto(id=None, nome=None):
         if id is not None:
@@ -36,6 +60,8 @@ class Dicionario:
                     return produto
         
         return "Produto não encontrado"
+    
+    
         
 
 
